64d125f26affef933e8cc5696339ffa42023bc5d
Matthew Hodali <matthew13@meet.mit.edu>
2014-12-09 20:07:57 +0200

    website

--- Lab2/pollingsite/polls/views.py
+++ Lab2/pollingsite/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from models import Poll, Choice
+# Create your views here(functions) here.
+#Remember each function/view the first argument/input has to be request
+def index(request):
+	return render(request,'firstcslecture.html',{"my_poll_num": poll_id}
